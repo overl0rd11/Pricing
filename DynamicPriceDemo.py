db55a29edd4f3452d2a3190dd98c8f540a0dbca5
overl0rd11 <akaul55@gmail.com>
2017-01-20 08:51:28 +0530

    Update DynamicPriceDemo.py

--- DynamicPriceDemo.py
+++ DynamicPriceDemo.py
@@ -8,16 +8,20 @@ from random import randrange
 import warnings
 import numpy as np
 from collections import Counter
+import sys
+"""this df contains our product, with 4 labels i.e. possible prices: 70,80,90,100"""
 
 df = {'70':[], '80':[], '90':[],'100':[]}
 
 
-increment = 70
-increment2=200
 flag=0
 flag2=0
 
-
+"""Generating the data here: Price    - >    Sales Range
+                              100              [70,79]
+                                90              [85, 114]
+                                80             [120, 149]
+                                70             [160, 179] """
 while (flag<160):
     for i in df:
         if (i=='70'):
@@ -60,16 +64,20 @@ def maxprofit(data):
             profit.append(j*(int(i)-cost_price))
     return max(profit)
     
-
-            
+""" Its important to enter a sales value between 70 to 180 and price between 70 to 100, might want to print out the data to see what it looks like,
+We have smaller sales numbers (around 70-79) for 100 Rs. and then sales increases for 90 Rs. and so on. Since, the price can be 70,80,90 or 100. 
+Enter one of those and not an arbitrary number or overly large number that defeats the purpose of this program """
+        
+""" One example of a good input would be 72 sold units at 100 Rs. Now you need 100 units sold"""
+    
 sold_today = input("Enter how many units you sold today:")
 sell_price = input("what price did you sell them at?:")
-
+cont = 0
 today_profit = sold_today*(sell_price - cost_price)
 if (maxprofit(df) <= today_profit):
     cont = input("You're doing better than or equal to the possible max profit you can attain using the algorithm already, it is recommended you continue with this and document this data so that after a while the algorithm can be retrained for better predictions, if you want to exit now, enter 1:")
-    if (cont):
-        exit(0)
+if (cont):
+    sys.exit() 
 
 want_sell = input("Enter how many total units you want to sell, please enter a realistic number:(max units sold till now are 179 at 70Rs/unit according to the data):")
     
@@ -85,3 +93,6 @@ print ("Max possible profit of the dataset is:",maxprofit(df))
 print ("Today's profit is:", today_profit)
 print ("Expected profit from new sales number:",predicted_profit)
 print(str(percentage_discount) + "% is the required discount to increase sales")
+
+
+#CODE IS WORKING BUT MAXPROFIT IS NOT. FIX THAT!
